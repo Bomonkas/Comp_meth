243a77548839a2b14b48656be8708137803bd0f2
vboled <56510659+vboled@users.noreply.github.com>
2019-10-27 00:28:40 +0300

    All is done

--- project/src/iterative.cpp
+++ project/src/iterative.cpp
@@ -4,21 +4,21 @@ TYPE *yacoby_iter(TYPE **A, TYPE *b, const int size)
 {
     TYPE *x = new TYPE[size];
     TYPE *x1 = new TYPE[size];
-    TYPE **B = new TYPE *[size];
+    TYPE **C = new TYPE *[size];
 
     for (int i = 0; i < size; i++)
     {
-        B[i] = new TYPE[size];
+        C[i] = new TYPE[size];
         for (int j = 0; j < size; j++)
         {
             if (i != j)
-                B[i][j] = -A[i][j] / A[i][i];
+                C[i][j] = -A[i][j] / A[i][i];
             else
-                B[i][i] = 0;
+                C[i][i] = 0;
         }
         x[i] = b[i];
     }
-    TYPE norm = norm_1_m(B, size);
+    TYPE norm = norm_1_m(C, size);
     TYPE eps1 = (1 - norm) / norm * eps;
     TYPE nev = norm_1_v(diff_v(x1, x, size), size);
     int h = 0;
@@ -29,7 +29,7 @@ TYPE *yacoby_iter(TYPE **A, TYPE *b, const int size)
             x1[i] = 0;
 			for (int j = 0; j < size; j++)
             {
-                if (i !=j )
+                if (i != j)
 				    x1[i] += -A[i][j] / A[i][i] * x[j];
             }
 			x1[i] += b[i] / A[i][i];
@@ -40,50 +40,56 @@ TYPE *yacoby_iter(TYPE **A, TYPE *b, const int size)
     cout << setprecision(10) << "|"  << setw(13) << norm_1_v(diff_v(x1, x, size), size) 
     << "|" << setw(15) << eps1 << "|"<< setw(13) << h << "|" << setw(15) 
     << norm << "|"<< setw(12) << (int)(log(eps * (1 - norm) / nev) / log(norm)) << "|" << endl;
-    delete_m(B, size);
+    delete_m(C, size);
     delete[] x1;
     return (x);
 }
 
-TYPE    **relax_m(TYPE **A, const int size, const TYPE w)
+TYPE **relax_m(TYPE **A, const int size, const TYPE w)
 {
-    TYPE **C;
-    TYPE **D;
-    TYPE **L;
+    TYPE *x = new TYPE[size];
+    TYPE **C = new TYPE*[size];
+    TYPE sum;
 
-    D = new TYPE*[size];
-    L = new TYPE*[size];
-    C = new TYPE*[size];
     for (int i = 0; i < size; i++)
     {
         C[i] = new TYPE[size];
-        D[i] = new TYPE[size];
-        L[i] = new TYPE[size];
+        x[i] = 0;
     }
-    
-    
+    for (int h = 0; h < size; h++)
+    {
+        x[h] = 1;
+        if (h != 0)
+            x[h - 1] = 0;
+        for (int i = 0; i < size; i++)
+		{
+            C[i][h] = 0;
+            sum = 0;
+			for (int j = 0; j < size; j++)
+            {
+                if (j < i)
+                    sum += A[i][j] * C[j][h];
+                else if(i != j)
+				    sum += A[i][j] * x[j];
+            }
+			C[i][h] = (1 - w) * C[i][h] - w * sum / A[i][i];
+        }
+    }
+    delete[]x;
+    return (C);
 }
 
 TYPE *zey_iter(TYPE **A, TYPE *b, const int size)
 {
-    TYPE **B = new TYPE *[size];
+    TYPE **C;
     TYPE *x = new TYPE[size];
     TYPE *x1 = new TYPE[size];
     TYPE *tmp = new TYPE[size];
 
     for (int i = 0; i < size; i++)
-    {
-        B[i] = new TYPE[size];
-        for (int j = 0; j < size; j++)
-        {
-            if (i != j)
-                B[i][j] = -A[i][j] / A[i][i];
-            else
-                B[i][i] = 0;
-        }
         x[i] = b[i];
-    }
-    TYPE norm = norm_1_m(B, size);
+    C = relax_m(A, size, 1);
+    TYPE norm = norm_1_m(C, size);
     TYPE eps1 = (1 - norm) / norm * eps;
     TYPE nev = norm_1_v(diff_v(x1, x, size), size);
     int h = 0;
@@ -109,7 +115,7 @@ TYPE *zey_iter(TYPE **A, TYPE *b, const int size)
     cout << setprecision(10) << "|"  << setw(13) << norm_1_v(diff_v(x1, x, size), size) 
     << "|" << setw(15) << eps1 << "|"<< setw(13) << h << "|" << setw(15) 
     << norm << "|"<< setw(12) << (int)(log(eps * (1 - norm) / nev) / log(norm)) << "|" << endl;
-    delete_m(B, size);
+    delete_m(C, size);
     delete[] x1;
     delete[] tmp;
     return (x);
@@ -117,24 +123,15 @@ TYPE *zey_iter(TYPE **A, TYPE *b, const int size)
 
 TYPE *rel_iter(TYPE **A, TYPE *b, const int size, const TYPE w)
 {
-    TYPE **B = new TYPE *[size];
+    TYPE **C;
     TYPE *x = new TYPE[size];
     TYPE *x1 = new TYPE[size];
     TYPE *tmp = new TYPE[size];
 
     for (int i = 0; i < size; i++)
-    {
-        B[i] = new TYPE[size];
-        for (int j = 0; j < size; j++)
-        {
-            if (i != j)
-                B[i][j] = -A[i][j] / A[i][i];
-            else
-                B[i][i] = 0;
-        }
         x[i] = b[i];
-    }
-    TYPE norm = norm_1_m(B, size);
+    C = relax_m(A, size, w);
+    TYPE norm = norm_1_m(C, size);
     TYPE eps1 = (1 - norm) / norm * eps;
     TYPE nev = norm_1_v(diff_v(x1, x, size), size);
     int h = 0;
@@ -146,12 +143,11 @@ TYPE *rel_iter(TYPE **A, TYPE *b, const int size, const TYPE w)
 			for (int j = 0; j < size; j++)
             {
                 if (j < i)
-                    x1[i] += -A[i][j] / A[i][i] * tmp[j];
+                    x1[i] -= w * A[i][j] / A[i][i] * tmp[j];
                 else if(i != j)
-				    x1[i] += -A[i][j] / A[i][i] * x[j];
+				    x1[i] -= w * A[i][j] / A[i][i] * x[j];
             }
-			x1[i] += b[i] / A[i][i];
-            x1[i] = x1[i] + (w - 1) * (x1[i] - x[i]);
+			x1[i] += w * b[i] / A[i][i] + (1 - w) * x[i];
             tmp[i] = x1[i];
         }
 		swap_v(&x1, &x);
@@ -160,7 +156,7 @@ TYPE *rel_iter(TYPE **A, TYPE *b, const int size, const TYPE w)
     cout << "|" << setw(13) << w  << setprecision(10) << "|"  << setw(13) << norm_1_v(diff_v(x1, x, size), size) 
     << "|" << setw(15) << eps1 << "|"<< setw(13) << h << "|" << setw(15) 
     << norm << "|" << setw(12) << (int)(log(eps * (1 - norm) / nev) / log(norm)) << "|" << endl;
-    delete_m(B, size);
+    delete_m(C, size);
     delete[] x1;
     delete[] tmp;
     return (x);
@@ -168,23 +164,23 @@ TYPE *rel_iter(TYPE **A, TYPE *b, const int size, const TYPE w)
 
 TYPE *simple_iter(TYPE **A, TYPE *b, const int size, const TYPE tau)
 {
-    TYPE **B = new TYPE *[size];
+    TYPE **C = new TYPE *[size];
     TYPE *x = new TYPE[size];
     TYPE *x1 = new TYPE[size];
 
     for (int i = 0; i < size; i++)
     {
-        B[i] = new TYPE[size];
+        C[i] = new TYPE[size];
         for (int j = 0; j < size; j++)
         {
-            B[i][j] = -tau * A[i][j];
-            if (i == j)
-                B[i][j] += 1;        
+            C[i][j] = -tau * A[i][j];     
         }
+        C[i][i] += 1;
         x[i] = b[i];
     }
+
     int h = 0;
-    TYPE norm = norm_1_m(B, size);
+    TYPE norm = norm_1_m(C, size);
     TYPE eps1 = (1 - norm) / norm * eps;
     TYPE nev = norm_1_v(diff_v(x1, x, size), size);
 	while (norm_1_v(diff_v(x1, x, size), size) > eps1)
@@ -193,7 +189,12 @@ TYPE *simple_iter(TYPE **A, TYPE *b, const int size, const TYPE tau)
 		{
             x1[i] = 0;
 			for (int j = 0; j < size; j++)
-				x1[i] += B[i][j] * x[j];
+            {
+                if (i != j)
+                    x1[i] += -tau * A[i][j] * x[j];
+                else
+                    x1[i] += (-tau * A[i][j] + 1) * x[j];
+            }
 			x1[i] += b[i] * tau;
         }
         swap_v(&x1, &x);
@@ -201,8 +202,8 @@ TYPE *simple_iter(TYPE **A, TYPE *b, const int size, const TYPE tau)
 	} 
     cout << setprecision(10) << "|" << setw(13) << tau  << "|"  << setw(13) << norm_1_v(diff_v(x1, x, size), size) 
     << "|" << setw(15) << eps1 << "|"<< setw(13) << h << "|" << setw(15) 
-    << norm_1_m(B, size) << "|"<< setw(12) << (int)(log(eps * (1 - norm) / nev) / log(norm)) << "|" << endl;
-    delete_m(B, size);
+    << norm << "|"<< setw(12) << (int)(log(eps * (1 - norm) / nev) / log(norm)) << "|" << endl;
+    delete_m(C, size);
     delete[] x1;
     return (x);
 }
